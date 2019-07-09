ba6fdfbb7d12681d9bdc365a61bebd8f6585b3c3
Christian Awelakoue <awelakoue@gmail.com>
2019-07-09 04:50:40 +0200

    create components for cmspage

--- src/app/cmspage/cmspage.module.ts
+++ src/app/cmspage/cmspage.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CmspageRoutingModule } from './cmspage-routing.module';
+import { PageComponent } from './page/page.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PageComponent, ContactFormComponent],
   imports: [
     CommonModule,
     CmspageRoutingModule
