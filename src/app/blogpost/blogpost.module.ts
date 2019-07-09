4d845703f0fe9720915dc33b344b279c6f71c863
Christian Awelakoue <awelakoue@gmail.com>
2019-07-09 04:49:05 +0200

    create components for blogpost

--- src/app/blogpost/blogpost.module.ts
+++ src/app/blogpost/blogpost.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BlogpostRoutingModule } from './blogpost-routing.module';
+import { BlogpostFeaturedComponent } from './blogpost-featured/blogpost-featured.component';
+import { BlogpostListComponent } from './blogpost-list/blogpost-list.component';
+import { BlogpostDetailComponent } from './blogpost-detail/blogpost-detail.component';
+import { BlogpostRecentComponent } from './blogpost-recent/blogpost-recent.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [BlogpostFeaturedComponent, BlogpostListComponent, BlogpostDetailComponent, BlogpostRecentComponent, CategoriesComponent],
   imports: [
     CommonModule,
     BlogpostRoutingModule
