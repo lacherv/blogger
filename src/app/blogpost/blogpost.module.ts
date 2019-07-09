edb6583a6313b55c714b01bdb410e9800979d54e
Christian Awelakoue <awelakoue@gmail.com>
2019-07-09 04:45:33 +0200

    create modules

---
+++ src/app/blogpost/blogpost.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BlogpostRoutingModule } from './blogpost-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BlogpostRoutingModule
+  ]
+})
+export class BlogpostModule { }
